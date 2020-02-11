2b95413183f58b8df826f478e1bf620022b39955
koharup <anandpita18@icloud.com>
2020-02-11 17:20:30 +0900

    最初のアプリ終わり

---
+++ app/src/main/java/app/sano/picchi/firstapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package app.sano.picchi.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun changeText (view: View){
+        (view as Button).text = "クリックされました"
+    }
+}
